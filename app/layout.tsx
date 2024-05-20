19e87f0a26dbf533d8cf7b619e13f98b4aea65ea
Ibrahim <ibrahim@ibrahims-mbp.home>
2024-05-20 08:57:06 -0400

    Enviorment Setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Villati",
+  description:
+    "Experience one-of-a-kind stays and create memories in extraordinary homes around the world.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="text-[#F5F5DC] text-2xl">HELLO VILLATI</div>;
+}
